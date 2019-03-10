a837a1786e57962f1be3e9f07d45faa8dac92114
aaronalbright <aaron.albright38@gmail.com>
2019-03-09 16:32:14 -0800

    Cleaning css and uneeded files

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,67 +1,65 @@
 import { appendBracket } from './modules';
-import PollModal from './modules/popup';
-import { svg } from './modules/svg';
 import { addFinal } from './modules/helpers';
 
 document.addEventListener(
   'DOMContentLoaded',
   function() {
     overrideDefault();
-    $('.champion .plate').html(svg);
 
     fetch(
       'https://gist.githubusercontent.com/aaronalbright/f343f4f6306a9f4d0788797ddf208c0e/raw/d9cb1b0686c1b19f1da9d5e8e46e37fa8252362f/bracket.json'
     )
-    .then(res => res.json())
-    .then(bracket => {
-      let round = appendBracket(bracket);
-      let final = false;
+      .then(res => res.json())
+      .then(bracket => {
+        let round = appendBracket(bracket);
+        let final = false;
 
-      let currentRound = round.slice(-1);
+        let currentRound = round.slice(-1);
 
-      $(`[class*="${currentRound}"]`).addClass('current');
+        $(`[class*="${currentRound}"]`).addClass('current');
 
-      currentRound == "final" ? (final = true) : (final = false);
+        currentRound == 'final' ? (final = true) : (final = false);
 
-      if (final) {
-        addFinal("Bocas House");
-      }
+        if (currentRound.includes('semi') || currentRound == 'final') {
+          $('.mm-container.champion').addClass('final-four');
+        }
 
-      if (!$('[class*="round1"]').hasClass('current')) {
-        // Addes green color to previus match ups
-        $('.current .team').each(function () {
-          let prevMatchup = $(this)
-            .parents()
-            .find('[class*="round"]:not(.current) .matchup .team');
+        if (final) {
+          addFinal('Bocas House');
+        }
 
-          for (let each of Array.from(prevMatchup)) {
-            if (each.innerText === this.innerText) $(each).addClass('winner');
-          }
-        });
-      }
-      if (window.innerWidth <= 700) {
-        $('.champion--congrats').removeClass('label');
+        if (!$('[class*="round1"]').hasClass('current')) {
+          // Addes green color to previus match ups
+          $('.current .team').each(function() {
+            let prevMatchup = $(this)
+              .parents()
+              .find('[class*="round"]:not(.current) .matchup .team');
 
-        if (!final) {
-          $(window).scroll(function () {
-            let scroll = document
-              .querySelector('.logo-container')
-              .getBoundingClientRect();
-            let el = $('.how-to');
-            if (scroll.bottom < 160) {
-              el.addClass('how-to_visible');
-            } else {
-              el.removeClass('how-to_visible');
+            for (let each of Array.from(prevMatchup)) {
+              if (each.innerText === this.innerText) $(each).addClass('winner');
             }
           });
         }
-      }
-      else {
-        if (!final) {
-          $('.how-to > p > span').text('Click');
+        if (window.innerWidth <= 700) {
+          if (!final) {
+            $(window).scroll(function() {
+              let scroll = document
+                .querySelector('.logo-container')
+                .getBoundingClientRect();
+              let el = $('.how-to');
+              if (scroll.bottom < 160) {
+                el.addClass('how-to_visible');
+              } else {
+                el.removeClass('how-to_visible');
+              }
+            });
+          }
+        } else {
+          if (!final) {
+            $('.how-to > p > span').text('Click');
+          }
         }
-      }
-    });
+      });
 
     fetch(
       'https://gist.githubusercontent.com/aaronalbright/c9b0a474e18fc7cedd877de7037b8e02/raw/613cb79a946b1105bd4930228196334fe23d2ffb/pollData.json'
